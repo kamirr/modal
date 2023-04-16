b4ddec6b6408194e514c3f1c21a0cb922078f370
Kamil Koczurek <koczurekk@gmail.com>
2023-04-16 09:16:47 +0200

    biquad filter expanded
    * allow switching between Q and BW parameters
    * added support for HP, LP, BP and notch.

--- src/compute/node/filters/mod.rs
+++ src/compute/node/filters/mod.rs
@@ -1,13 +1,15 @@
 use atomic_enum::atomic_enum;
-use atomic_float::AtomicF32;
-use eframe::egui::{ComboBox, DragValue};
+use eframe::egui::ComboBox;
 use serde::{Deserialize, Serialize};
 
-use super::{Input, Node, NodeConfig, NodeEvent, NodeList};
+use super::{
+    inputs::{freq::FreqInput, positive::PositiveInput},
+    Input, InputUi, Node, NodeConfig, NodeEvent, NodeList,
+};
 use std::{
     f32::consts::PI,
     sync::{
-        atomic::{AtomicU32, AtomicUsize, Ordering},
+        atomic::{AtomicUsize, Ordering},
         Arc,
     },
 };
@@ -16,6 +18,9 @@ use std::{
 #[derive(PartialEq)]
 enum BiquadTy {
     Lpf,
+    Hpf,
+    Bpf,
+    Notch,
 }
 
 impl Serialize for AtomicBiquadTy {
@@ -36,82 +41,98 @@ impl<'de> Deserialize<'de> for AtomicBiquadTy {
     }
 }
 
+#[atomic_enum]
+#[derive(PartialEq, Serialize, Deserialize)]
+enum ParamTy {
+    Q,
+    Bw,
+}
+
+impl Serialize for AtomicParamTy {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for AtomicParamTy {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        AtomicUsize::deserialize(deserializer).map(|inner| AtomicParamTy(inner))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct BiquadConfig {
-    ty: AtomicBiquadTy,
-    f0: AtomicU32,
-    q: AtomicF32,
+    filt_ty: AtomicBiquadTy,
+    param_ty: AtomicParamTy,
 }
 
 impl BiquadConfig {
-    fn new(ty: BiquadTy, f0: u32, q: f32) -> Self {
+    fn new(filt_ty: BiquadTy, param_ty: ParamTy) -> Self {
         BiquadConfig {
-            ty: AtomicBiquadTy::new(ty),
-            f0: AtomicU32::new(f0),
-            q: AtomicF32::new(q),
-        }
-    }
-
-    fn coeffs(&self) -> ([f32; 3], [f32; 3]) {
-        let ty = self.ty.load(Ordering::Relaxed);
-        let f0 = self.f0.load(Ordering::Relaxed);
-        let q = self.q.load(Ordering::Relaxed);
-
-        let w0 = 2.0 * PI * (f0 as f32) / 44100.0;
-        let alpha = w0.sin() / 2.0 / q;
-
-        match ty {
-            BiquadTy::Lpf => (
-                [1.0 + alpha, -2.0 * w0.cos(), 1.0 - alpha],
-                [
-                    (1.0 - w0.cos()) / 2.0,
-                    1.0 - w0.cos(),
-                    (1.0 - w0.cos()) / 2.0,
-                ],
-            ),
+            filt_ty: AtomicBiquadTy::new(filt_ty),
+            param_ty: AtomicParamTy::new(param_ty),
         }
     }
 }
 
 impl NodeConfig for BiquadConfig {
     fn show(&self, ui: &mut eframe::egui::Ui) {
-        let mut ty = self.ty.load(Ordering::Acquire);
-        let mut f0 = self.f0.load(Ordering::Acquire);
-        let mut q = self.q.load(Ordering::Acquire);
+        let mut filt_ty = self.filt_ty.load(Ordering::Acquire);
+        let mut param_ty = self.param_ty.load(Ordering::Acquire);
 
-        ComboBox::from_label("")
-            .selected_text(format!("{ty:?}"))
+        ComboBox::from_id_source("foo")
+            .selected_text(format!("{filt_ty:?}"))
             .show_ui(ui, |ui| {
-                ui.selectable_value(&mut ty, BiquadTy::Lpf, "Lpf");
+                ui.selectable_value(&mut filt_ty, BiquadTy::Lpf, "Lpf");
+                ui.selectable_value(&mut filt_ty, BiquadTy::Hpf, "Hpf");
+                ui.selectable_value(&mut filt_ty, BiquadTy::Bpf, "Bpf");
+                ui.selectable_value(&mut filt_ty, BiquadTy::Notch, "Notch");
             });
-        ui.add(DragValue::new(&mut f0).clamp_range(0.0..=20000.0));
-        ui.add(DragValue::new(&mut q).clamp_range(0.1..=10.0));
+        ComboBox::from_id_source("bar")
+            .selected_text(format!("{param_ty:?}"))
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut param_ty, ParamTy::Q, "Q");
+                ui.selectable_value(&mut param_ty, ParamTy::Bw, "BW");
+            });
 
-        self.ty.store(ty, Ordering::Release);
-        self.f0.store(f0, Ordering::Release);
-        self.q.store(q, Ordering::Release);
+        self.filt_ty.store(filt_ty, Ordering::Release);
+        self.param_ty.store(param_ty, Ordering::Release);
     }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct Biquad {
     config: Arc<BiquadConfig>,
+    f0: Arc<FreqInput>,
+    q: Arc<PositiveInput>,
+    bw: Arc<PositiveInput>,
+    param_ty: ParamTy,
     in_hist: [f32; 3],
     out_hist: [f32; 2],
 }
 
 impl Biquad {
     fn new() -> Self {
-        let config = BiquadConfig::new(BiquadTy::Lpf, 440, 1.0);
+        let config = BiquadConfig::new(BiquadTy::Lpf, ParamTy::Q);
         Biquad {
             config: Arc::new(config),
+            f0: Arc::new(FreqInput::new(440.0)),
+            q: Arc::new(PositiveInput::new(0.707)),
+            bw: Arc::new(PositiveInput::new(1.0)),
+            param_ty: ParamTy::Q,
             in_hist: [0.0; 3],
             out_hist: [0.0; 2],
         }
     }
 
-    fn next(&mut self, input: f32) {
-        let (a, b) = self.config.coeffs();
+    fn next(&mut self, input: f32, f0: Option<f32>, param: Option<f32>) {
+        let (a, b) = self.coeffs(f0, param);
 
         self.in_hist = [self.in_hist[1], self.in_hist[2], input];
         let out = (b[0] / a[0]) * self.in_hist[2]
@@ -122,14 +143,60 @@ impl Biquad {
 
         self.out_hist = [self.out_hist[1], out];
     }
+
+    fn coeffs(&self, f0: Option<f32>, param: Option<f32>) -> ([f32; 3], [f32; 3]) {
+        let ty = self.config.filt_ty.load(Ordering::Relaxed);
+        let param_ty = self.config.param_ty.load(Ordering::Relaxed);
+
+        let f0 = f0.unwrap_or(self.f0.value());
+        let param = param.unwrap_or(match param_ty {
+            ParamTy::Q => self.q.value(),
+            ParamTy::Bw => self.bw.value(),
+        });
+
+        let w0 = 2.0 * PI * (f0 as f32) / 44100.0;
+        let w0sin = w0.sin();
+        let w0cos = w0.cos();
+
+        let alpha = match param_ty {
+            ParamTy::Q => w0sin / 2.0 / param,
+            ParamTy::Bw => w0sin * (2f32.ln() / 2.0 * param * w0 / w0sin).sinh(),
+        };
+
+        match ty {
+            BiquadTy::Lpf => (
+                [1.0 + alpha, -2.0 * w0cos, 1.0 - alpha],
+                [(1.0 - w0cos) / 2.0, 1.0 - w0cos, (1.0 - w0cos) / 2.0],
+            ),
+            BiquadTy::Hpf => (
+                [1.0 + alpha, -2.0 * w0cos, 1.0 - alpha],
+                [(1.0 + w0cos) / 2.0, -1.0 - w0cos, (1.0 + w0cos) / 2.0],
+            ),
+            BiquadTy::Bpf => (
+                [1.0 + alpha, -2.0 * w0cos, 1.0 - alpha],
+                [alpha, 0.0, -alpha],
+            ),
+            BiquadTy::Notch => (
+                [1.0 + alpha, -2.0 * w0cos, 1.0 - alpha],
+                [1.0, -2.0 * w0cos, 1.0],
+            ),
+        }
+    }
 }
 
 #[typetag::serde]
 impl Node for Biquad {
     fn feed(&mut self, data: &[Option<f32>]) -> Vec<NodeEvent> {
-        self.next(data[0].unwrap_or(0.0));
+        self.next(data[0].unwrap_or(0.0), data[1], data[2]);
 
-        Default::default()
+        let new_param_ty = self.config.param_ty.load(Ordering::Relaxed);
+        if self.param_ty != new_param_ty {
+            self.param_ty = new_param_ty;
+
+            vec![NodeEvent::RecalcInputs(self.inputs())]
+        } else {
+            Default::default()
+        }
     }
 
     fn read(&self) -> f32 {
@@ -141,7 +208,14 @@ impl Node for Biquad {
     }
 
     fn inputs(&self) -> Vec<Input> {
-        vec![Input::new("sig")]
+        vec![
+            Input::new("sig"),
+            Input::with_default("f0", &self.f0),
+            match &self.param_ty {
+                ParamTy::Q => Input::with_default("Q", &self.q),
+                ParamTy::Bw => Input::with_default("BW", &self.bw),
+            },
+        ]
     }
 }
 
