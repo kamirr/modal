673c7df4becbe0f061f952c38aa5f399870200e2
Kamil Koczurek <koczurekk@gmail.com>
2023-05-12 12:00:49 +0200

    [graph] name label for disconnected inputs

--- src/graph.rs
+++ src/graph.rs
@@ -258,6 +258,7 @@ impl WidgetValueTrait for SynthValueType {
         let ui_inputs = user_state.node_ui_inputs.get(&node_id).unwrap();
         if let Some(input) = ui_inputs.get(param_name) {
             ui.horizontal(|ui| {
+                ui.label(param_name);
                 input.show_always(ui, *node_data.verbose.borrow());
                 input.show_disconnected(ui, *node_data.verbose.borrow());
             });
