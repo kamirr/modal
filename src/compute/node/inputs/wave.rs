a6101d84644d6a8d5f0b03a66299c36ca76848a7
Kamil Koczurek <koczurekk@gmail.com>
2023-05-25 14:28:11 +0200

    [basic::oscillator] Add knob for choosing shape

--- src/compute/node/inputs/wave.rs
+++ src/compute/node/inputs/wave.rs
@@ -1,9 +1,12 @@
 use atomic_float::AtomicF32;
-use eframe::egui;
+use eframe::epaint::ColorImage;
 use serde::{Deserialize, Serialize};
 use std::sync::atomic::Ordering;
 
-use crate::compute::{node::InputUi, Value};
+use crate::{
+    compute::{node::InputUi, Value},
+    util::load_image_from_path,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WaveInput {
@@ -20,13 +23,25 @@ impl WaveInput {
     pub fn as_f32(&self, recv: &Value) -> f32 {
         recv.as_float().unwrap_or(self.s.load(Ordering::Relaxed))
     }
+
+    fn wave_knob_image() -> ColorImage {
+        load_image_from_path("assets/knob.png")
+    }
+
+    fn wave_scale_image() -> ColorImage {
+        load_image_from_path("assets/shape-scale.png")
+    }
 }
 
 impl InputUi for WaveInput {
     fn show_disconnected(&self, ui: &mut eframe::egui::Ui, _verbose: bool) {
         let mut s = self.s.load(Ordering::Acquire);
 
-        ui.add(egui::Slider::new(&mut s, 0.0..=1.0));
+        ui.add(egui_knobs::Knob::new(
+            &mut s,
+            Self::wave_knob_image,
+            Self::wave_scale_image,
+        ));
 
         self.s.store(s, Ordering::Release);
     }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -307,3 +307,11 @@ pub fn toggle_button(label: &str, state: bool) -> eframe::egui::Button {
         eframe::egui::Button::new(label)
     }
 }
+
+pub fn load_image_from_path(path: impl AsRef<std::path::Path>) -> eframe::egui::ColorImage {
+    let image = image::io::Reader::open(path).unwrap().decode().unwrap();
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    eframe::egui::ColorImage::from_rgba_unmultiplied(size, pixels.as_slice())
+}
