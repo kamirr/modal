6317ae3eb63be47d7c8173e4aa32b3e709ca9ddb
Kamil Koczurek <koczurekk@gmail.com>
2023-08-07 17:43:28 +0200

    [effects] add bits effect for quantizing the signal

--- src/compute/node/effects/mod.rs
+++ src/compute/node/effects/mod.rs
@@ -1,5 +1,6 @@
 use super::{Node, NodeList};
 
+pub mod bits;
 pub mod chorus;
 pub mod clip;
 pub mod glide;
@@ -10,6 +11,7 @@ pub struct Effects;
 impl NodeList for Effects {
     fn all(&self) -> Vec<(Box<dyn Node>, String, Vec<String>)> {
         vec![
+            (bits::bits(), "Bits".into(), vec!["Effect".into()]),
             (chorus::chorus(), "Chorus".into(), vec!["Effect".into()]),
             (clip::clip(), "Clip".into(), vec!["Effect".into()]),
             (glide::glide(), "Glide".into(), vec!["Effect".into()]),
